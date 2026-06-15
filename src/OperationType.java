3541a8b52d53f3304e1e8d1fa28350e7bfa4fdc6
Sii <aranedabastian2019@gmail.com>
2026-06-15 01:47:10 -0400

    Delete src directory

--- src/OperationType.java
+++
@@ -1,8 +0,0 @@
-// tipos de operaciones que se pueden hacer sobre el inventario
-public enum OperationType {
-    PURCHASE,   // compra o ingreso de componentes
-    QUERY,      // consulta de disponibilidad
-    LEND,       // prestamo a un estudiante
-    RECEIVE,    // devolucion de un componente
-    DISPOSE     // baja por daño, perdida u obsolescencia
-}
